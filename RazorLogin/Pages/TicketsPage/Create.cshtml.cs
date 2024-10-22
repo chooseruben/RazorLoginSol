39fe1ab0a3274099f6f5052cc8073454814c1ef2
Mark Saldana <04mark.s@gmail.com>
2024-10-22 18:00:38 -0500

    Added Customer Page with functioning information edits.

---
+++ RazorLogin/Pages/TicketsPage/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorLogin.Models;
+
+namespace RazorLogin.Pages.TicketsPage
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorLogin.Models.ZooDbContext _context;
+
+        public CreateModel(RazorLogin.Models.ZooDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["PurchaseId"] = new SelectList(_context.Purchases, "PurchaseId", "PurchaseId");
+            return Page();
+        }
+
+        [BindProperty]
+        public Ticket Ticket { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Tickets.Add(Ticket);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
